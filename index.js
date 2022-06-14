676281839fbfc66a98bf93a300661c458e1e05d2
sireclifford <dev.sire.clifford@gmail.com>
2022-06-14 10:23:59 +0000

    implemented js & connected sections to menu url

--- index.js
+++ index.js
@@ -1,30 +1,63 @@
-const overlay = document.querySelector('#menu-overlay-container');
-const wrapper = document.querySelector('#wrapper');
+const GotoProjects = () => {
+  const projectsLink = document.querySelector('#projects-link');
+  const overlay = document.querySelector('#menu-overlay-container');
+  const wrapper = document.querySelector('#wrapper');
 
-const About = () => {
-  wrapper.style.display = 'block';
-  overlay.style.display = 'none';
-  document.getElementById('about').scrollIntoView();
+  projectsLink.addEventListener('click', () => {
+    wrapper.style.display = 'block';
+    overlay.style.display = 'none';
+    document.getElementById('projects').scrollIntoView();
+  });
 };
 
-const Projects = () => {
-  wrapper.style.display = 'block';
-  overlay.style.display = 'none';
-  document.getElementById('projects').scrollIntoView();
+const GotoGettingStarted = () => {
+  const gettingStartedLink = document.querySelector('#getting-started-link');
+  const overlay = document.querySelector('#menu-overlay-container');
+  const wrapper = document.querySelector('#wrapper');
+
+  gettingStartedLink.addEventListener('click', () => {
+    wrapper.style.display = 'block';
+    overlay.style.display = 'none';
+    document.getElementById('getting-started').scrollIntoView();
+  });
 };
 
-const GettingStarted = () => {
-  wrapper.style.display = 'block';
-  overlay.style.display = 'none';
-  document.getElementById('getting-started').scrollIntoView();
+const GotoAbout = () => {
+  const aboutLink = document.querySelector('#about-link');
+  const overlay = document.querySelector('#menu-overlay-container');
+  const wrapper = document.querySelector('#wrapper');
+
+  aboutLink.addEventListener('click', () => {
+    wrapper.style.display = 'block';
+    overlay.style.display = 'none';
+    document.getElementById('about').scrollIntoView();
+  });
 };
 
-function openMenu() {
-  overlay.style.display = 'grid';
-  wrapper.style.display = 'none';
-}
+const OpenMenu = () => {
+  const openMenuIcon = document.querySelector('#open-menu-icon');
+  const overlay = document.querySelector('#menu-overlay-container');
+  const wrapper = document.querySelector('#wrapper');
 
-function closeMenu() {
-  overlay.style.display = 'none';
-  wrapper.style.display = 'block';
-}
+  openMenuIcon.addEventListener('click', () => {
+    overlay.style.display = 'grid';
+    wrapper.style.display = 'none';
+  });
+};
+
+const CloseMenu = () => {
+  const closeMenuIcon = document.querySelector('#close-menu-icon');
+  const overlay = document.querySelector('#menu-overlay-container');
+  const wrapper = document.querySelector('#wrapper');
+
+  closeMenuIcon.addEventListener('click', () => {
+    overlay.style.display = 'none';
+    wrapper.style.display = 'block';
+  });
+};
+
+OpenMenu();
+CloseMenu();
+GotoProjects();
+GotoGettingStarted();
+GotoAbout();
